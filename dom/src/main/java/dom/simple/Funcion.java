b88f6f4a645da23e445c291d2f88762f6a23fab0
sbenigar <sabrinabenigar@hotmail.com>
2014-07-20 16:38:56 -0300

    Clase Funcion con atributo 'Nombre'y anotaciones de persistencia

--- dom/src/main/java/dom/simple/Funcion.java
+++ dom/src/main/java/dom/simple/Funcion.java
@@ -1,11 +1,31 @@
 package dom.simple;
 
-import java.util.List;
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.Persistent;
 
 import org.apache.isis.applib.annotation.MemberOrder;
 
 public class Funcion {
 	
+	// {{ Nombre (property)
+	private E_funciones nombre;
 
 
+	@Column(allowsNull = "true")
+	@MemberOrder(sequence = "1")
+	@Persistent
+	public E_funciones getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(final E_funciones nombre) {
+		this.nombre = nombre;
+	}
+	// }}
+
+	public enum E_funciones{
+		SECRETARIO, PRECEPTOR, PROFESOR, DIRECTOR;
+	}
 }
+
+
