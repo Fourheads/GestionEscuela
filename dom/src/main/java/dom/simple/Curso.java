fc958fa0254c8a04eef1ff0491f367042de7e7e1
Mingorth <Mingorth@gmail.com>
2014-07-26 17:54:53 -0300

    Creaa la clase curso, faltan anotaciones JDO

---
+++ dom/src/main/java/dom/simple/Curso.java
@@ -0,0 +1,53 @@
+package dom.simple;
+
+import java.util.List;
+
+public class Curso {
+	
+	private Turno Turno;
+	private int Año;
+	private String Division;
+	public String getDivision() {
+		return Division;
+	}
+	public void setDivision(String division) {
+		Division = division;
+	}
+	private List<Materia> Materias;
+	private List<Alumno> Alumnos;
+	private Preceptor Preceptor;
+	public Turno getTurno() {
+		return Turno;
+	}
+	public void setTurno(Turno turno) {
+		Turno = turno;
+	}
+	public int getAño() {
+		return Año;
+	}
+	public void setAño(int año) {
+		Año = año;
+	}
+	public List<Materia> getMaterias() {
+		return Materias;
+	}
+	public void setMaterias(List<Materia> materias) {
+		Materias = materias;
+	}
+	public List<Alumno> getAlumnos() {
+		return Alumnos;
+	}
+	public void setAlumnos(List<Alumno> alumnos) {
+		Alumnos = alumnos;
+	}
+	public Preceptor getPreceptor() {
+		return Preceptor;
+	}
+	public void setPreceptor(Preceptor preceptor) {
+		Preceptor = preceptor;
+	}
+	
+	
+	
+
+}
